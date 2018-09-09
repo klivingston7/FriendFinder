a7c0cd6a3fa3402c89449096f9e25c8e0be9f9d6
KLIV <wclivingston5@gmail.com>
2018-09-08 23:54:20 -0500

    update survey and modal

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,12 +1,9 @@
-// Your apiRoutes.js file should contain two routes:
-// A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
-// A POST routes /api/friends. This will be used to handle incoming survey results. 
-// This route will also be used to handle the compatibility logic.
 
 // Dependencies
 
 var friends = require("../data/friends.js");
 
+// Routes
 
 module.exports = function(app){
     // A GET route used to display a JSON of all possible friends.
@@ -16,7 +13,35 @@ module.exports = function(app){
     })
     // A POST route used to handle incoming survey results + compatibility logic. 
     app.post("/api/friends", function(req, res){
-        console.log(req.body);
-        res.send(req.body);
-    })
+      
+          var userData = req.body;
+          var userScores = userData.scores;
+          console.log(userData);
+          console.log(userScores);
+
+          var totalDiff = 0;
+          var match = {
+            name: "",
+            photo: "",
+            friendDiff: 500
+          };
+      
+          for (var i = 0; i < friends.length; i++) {
+            console.log(friends[i]);
+            totalDiff = 0;
+
+            for (var k = 0; k < friends[i].scores[k]; k++) {
+              totalDiff += Math.abs(parseInt(userScores[k]) - parseInt(friends[i].scores[k]));
+              if (totalDiff <= match.friendDiff) {
+                match.name = friends[i].name;
+                match.photo = friends[i].photo;
+                match.friendDiff = totalDiff;
+              }
+            }
+          }
+      
+          friends.push(userData);
+      
+          res.json(match);
+        });
 }
